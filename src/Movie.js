78e6bc8dc2df1ea6162ed896ac6eeaefef02dcab
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-04-05 19:24:35 -0500

    Refactor list of movies to Movie component

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
 export default function Movie(props){
     return (
         <div className="movie-item-style-2">
-            <img src="images/uploads/mv1.jpg" alt=""/>
+            <img src={props.img} alt=""/>
             <div className="mv-item-infor">
                 <h6><a href="moviesingle.html">{props.title}</a></h6>
                 <p className="rate"><i className="ion-android-star"></i><span>{props.rating}</span> /10</p>
